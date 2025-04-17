44036ae19db74b70de711fcf3e8b64516f5be441
Haizhou Ge <1352674740@qq.com>
2025-04-17 20:32:40 +0800

    fix vcs import

--- docker_build/workspace/install.sh
+++ docker_build/workspace/install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -ex
+
 SRC_DIR=$1
 
 # Prepare workspace and packages
-mkdir -p $SRC_DIR && cd $SRC_DIR
-pip install vcstool -i https://pypi.mirrors.ustc.edu.cn/simple
-cd AIRBOT_PLAY/src
-vcs import < airbot_app_build/airbot_app.repos
+apt install -y python3-pip python3-rosdep python3-vcstool python3-catkin-pkg git
+mkdir -p $SRC_DIR 
+vcs import $SRC_DIR < block_building_ros1.repos
 
 # Configure packages
+cd $SRC_DIR
 pushd airbot_play_moveit1_base
 chmod +x *.sh && ./config_basic.sh
 apt-get install ros-noetic-rviz ros-noetic-moveit -y
@@ -19,7 +21,7 @@ chmod +x *.sh && ./update.sh
 popd
 
 pushd airbot_play_gazebo/src/airbot_play_gazebo && chmod +x *.sh
-apt-get install ros-noetic-gazebo-plugins
+apt-get install ros-noetic-gazebo-plugins -y
 ./gazebo_config.sh
 ./gazebo_grasp_config.sh
 ./download_models.sh
@@ -31,7 +33,6 @@ pip install rosdepc -i https://pypi.mirrors.ustc.edu.cn/simple
 rosdepc init && rosdepc update
 rosdep install --from-path src --ignore-src -r -y
 
-apt-get install python3-catkin-tools || pip3 install catkin-tools catkin-tools-python
 catkin build
 
 echo "source $(pwd)/devel/setup."${SHELL##*/}"" >> ~/."${SHELL##*/}"rc
