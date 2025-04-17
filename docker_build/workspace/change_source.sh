7daa12047da3940703a3409caa12764443de8bf0
Haizhou Ge <1352674740@qq.com>
2025-04-17 19:57:05 +0800

    change ros source

--- docker_build/workspace/change_source.sh
+++ docker_build/workspace/change_source.sh
@@ -5,6 +5,10 @@
 # sed -i "s http://.*security.ubuntu.com http://repo.huaweicloud.com g" /etc/apt/sources.list.d/ubuntu.sources
 
 # <24.04
-sudo sed -i "s@http://.*archive.ubuntu.com@http://mirrors.huaweicloud.com@g" /etc/apt/sources.list
-sudo sed -i "s@http://.*security.ubuntu.com@http://mirrors.huaweicloud.com@g" /etc/apt/sources.list
+sed -i "s@http://.*archive.ubuntu.com@http://mirrors.huaweicloud.com@g" /etc/apt/sources.list
+sed -i "s@http://.*security.ubuntu.com@http://mirrors.huaweicloud.com@g" /etc/apt/sources.list
+
+rm /etc/apt/sources.list.d/ros1-latest.list
+sh -c 'echo "deb https://mirrors.huaweicloud.com/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt-get update
